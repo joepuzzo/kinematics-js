11b877dcbe430015b990ee1773e81d221d03257e
Joe Puzzo <jpuzzo@tesla.com>
2022-11-09 19:04:56 -0800

    updated to build individual files for functions

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,19 @@
 export default {
-  input: [`lib/index.js`, 'lib/forward.js', 'lib/inverse.js'],
+  input: [
+    `lib/index.js`,
+    'lib/forward.js',
+    'lib/inverse.js',
+    'lib/denavitHartenberg.js',
+    'lib/toRadians.js',
+    'lib/math.js',
+    'lib/round.js',
+    'lib/matrixDot.js',
+    'lib/matrixSubset.js',
+    'lib/inverse1_3.js',
+    'lib/debug.js',
+    'lib/roundMatrix.js',
+    'lib/euler.js',
+  ],
   output: [
     {
       dir: `dist/cjs`,
